5ed746f843750ff5b19ce818a6bed8f92e9e534a
DavidMalagonC <59100911+DavidMalagonC@users.noreply.github.com>
2020-01-16 17:35:06 -0500

    Add files via upload

--- dbConection.php
+++ dbConection.php
@@ -2,9 +2,9 @@
 //"localhost: 3306"
 
 $hotsdb = "localhost";    
-$db = "esmerald_freedcamp";   
-$dbUser = "esmerald";   
-$dbPassword = "v48oCNG88+opC+";    
+$db = "freedcamp";   
+$dbUser = "dbUser";   
+$dbPassword = "dbPassword";    
 $mysqli = new mysqli('localhost', $dbUser, $dbPassword, $db)
 or die ("Conexión denegada, el Servidor de Base de datos que solicitas NO EXISTE");
 $mysqli->query("SET NAMES 'utf8'");
